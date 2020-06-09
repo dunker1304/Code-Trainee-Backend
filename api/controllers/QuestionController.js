9242c4937a76480d45487bfd11ed6df6c4a765e7
kieuquynh <kieuquynh1234@gmail.com>
2020-06-09 11:39:33 +0700

    update question controller

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -4,8 +4,6 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-const CUSTOM = require('../../config/custom').custom
-const axios = require('axios');
 const QuestionComponent = require('../components/QuestionComponent')
 module.exports = {
 
@@ -25,17 +23,6 @@ module.exports = {
       // 'expected_output':ele['expectedOutput']
     }
 
-    // // //save test case
-    // let tc = {
-    //   question_id : 1,
-    //   input: '6\n1 2 3 4 5 7',
-    //   expected_output : 22
-    // }
-
-    //  await TestCase.create(tc)
-
-    //  res.send("oke");
-
     //get testcase of question
     let testCase = await TestCase.find({ questionId: questionId })
 
@@ -48,7 +35,7 @@ module.exports = {
 
     //check testcase
     let testCaseResult = [];
-    //  testCase.forEach(ele => {
+  
     for (let i = 0; i < testCase.length; i++) {
       submitData = {
         'language_id': languageId,
@@ -56,7 +43,7 @@ module.exports = {
         'stdin': testCase[i]['input'],
         'expected_output': testCase[i]['expectedOutput']
       }
-      console.log(submitData)
+     
       let submitResult = await QuestionComponent.submitQuestion(submitData);
 
       // một testcase xảy ra lỗi runtime or compile thì dừng lại ctr và trả ra lỗi
@@ -66,9 +53,6 @@ module.exports = {
 
       testCaseResult.push(submitResult);
     }
-
-
-    // });
 
     res.send(testCaseResult);
   }
