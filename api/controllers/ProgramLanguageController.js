fac0941451c43efb21990c4e42c7b1211dd717df
dunker1304 <dunker1304@gmail.com>
2020-08-17 00:02:52 +0700

    ;fix

--- api/controllers/ProgramLanguageController.js
+++ api/controllers/ProgramLanguageController.js
@@ -4,6 +4,7 @@ module.exports = {
       let { exerciseId } = req.params;
       let languages = await ProgramLanguage.find().populate("codeSnippets", {
         exerciseId: exerciseId,
+        isActive: true
       });
       res.json({
         success: true,
